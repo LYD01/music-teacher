8bbbe40b4d3ae761316c24a04fe0df2807b51a15
LD <lucasdrie@outlook.com>
2026-02-28 12:59:46 +1100

    dev: update git ignore + sync env vars with vercel

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,6 @@ import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
 config({ path: ".env.local" });
-config({ path: "src/.env.local" });
 
 export default defineConfig({
 	schema: "./src/lib/db/schema.ts",
